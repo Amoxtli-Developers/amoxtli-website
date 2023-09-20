dc0288e25ad081df2de545d69076b0a57e59253b
salomon <mtzelisalomon@gmail.com>
2023-09-20 12:22:40 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactNode, useLayoutEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
+import "./App.css";
+
+interface WrapperProps {
+  children: ReactNode;
+}
+
+const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return <>{children}</>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Wrapper>
     </div>
   );
 }
---
+++ src/routes/Service.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+
+function Service() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default Service;
